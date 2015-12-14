4c7a056fe29c718950b76cb5ac0be395edffde57
Tomás de Lima <tomasdelima@gmail.com>
2015-12-13 23:46:19 -0200

    Adds Angular Audio files

--- mobile/deploy.sh
+++ mobile/deploy.sh
@@ -1,16 +1,17 @@
 source ~/.bash_profile
 
 # Add key to deploy
-# keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
+# keytool -genkey -v -keystore badi-calendar.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 
 # Build the project
 cd ~/Projects/badi-calendar/mobile/
 ionic build android --release
-cd ~/Projects/badi-calendar/mobile/platforms/android/build/outputs/apk/
+
+export APK_PATH=~/Projects/badi-calendar/mobile/platforms/android/build/outputs/apk
 
 # Sign APK
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore android-release-unsigned.apk alias_name
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore badi-calendar.keystore $APK_PATH/android-release-unsigned.apk alias_name
 
 # Zipalign the APK
-mv BadiCalendar.apk BadiCalendar-old.apk
-~/Custom\ programs/android-sdk-linux/build-tools/20.0.0/zipalign -v 4 android-release-unsigned.apk BadiCalendar.apk
+mv $APK_PATH/BadiCalendar.apk $APK_PATH/BadiCalendar-old.apk 2> /dev/null
+zipalign -v 4 $APK_PATH/android-release-unsigned.apk $APK_PATH/BadiCalendar.apk
