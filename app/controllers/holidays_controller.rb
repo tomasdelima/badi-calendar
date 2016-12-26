3599feb08c9787191cea6089c5858a6eb3c41921
Tomás de Lima <tomasdelima@gmail.com>
2016-12-26 16:46:40 -0200

    Returns only the last updated holidays

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,15 +1,40 @@
 class HolidaysController < ApplicationController
   def index
-    render json: (Holiday.where("date < ?", Date.new(2064, 1, 1)).map do |h|
-      badi_date = h.to_badi
-      {
-        id: h.id,
-        date: h.date,
-        name: h.name,
-        year: badi_date.year,
-        month: badi_date.month,
-        day: badi_date.day
-      }
-    end.to_json)
+    @holidays = Holiday.where("date < ?", Date.new(2064, 1, 1))
+    if params[:last_updated_at]
+      holidaysWithDate
+    else
+      holidaysWithoutDate
+    end
+  end
+
+  private
+
+  def holidaysWithDate
+    @holidays = @holidays.where("updated_at > '#{params[:last_updated_at]}'")
+    holidaysToObject
+    render json: {time: Time.now.utc, data: @holidays.to_json}
+  end
+
+  def holidaysWithoutDate
+    holidaysToObject
+    render json: @holidays.to_json
+  end
+
+  def holidaysToObject
+    @holidays = @holidays.map do |holiday|
+      begin
+        badi_date = holiday.to_badi
+        {
+          id: holiday.id,
+          date: holiday.date,
+          name: holiday.name,
+          year: badi_date.year,
+          month: badi_date.month,
+          day: badi_date.day
+        }
+      rescue
+      end
+    end
   end
 end
